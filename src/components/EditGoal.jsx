47f0a9ab17ec4fd9f0e355e4041425ed3d189e83
Andreas Backström <mozzo242@gmail.com>
2025-01-03 19:51:54 +0100

    Add edit goal

---
+++ src/components/EditGoal.jsx
@@ -0,0 +1,75 @@
+import React, { useEffect, useState } from 'react'
+import { Button, Select, Label, TextInput } from "flowbite-react";
+import { db } from '../db';
+import { useLiveQuery } from "dexie-react-hooks";
+import { MdDelete } from "react-icons/md";
+
+function EditGoal(props) {
+    const goalInfo = useLiveQuery(async () => {
+        const goalInfo = await db.goals.get(props.id);
+        return goalInfo;
+    },[props.id]);
+    const [goalType, setGoalType] = useState();
+    const [goalName, setGoalName] = useState();
+    const [goal, setGoal] = useState();
+
+    useEffect(() => {
+      setGoalName(goalInfo?.name)
+      setGoalType(goalInfo?.type)
+      setGoal(goalInfo?.goal)
+  
+    }, [goalInfo])
+
+    const handleDeletion = () => {
+        console.log("DELETE!")
+        db.goals.delete(props.id)
+        props.onSuccessfulDeletion();
+    };
+
+    return (
+        <div>
+            <div className="flex flex-col gap-4">
+                <div>
+                    <div className="mb-2 block">
+                        <Label htmlFor="goal_name" value="Your goal name" />
+                    </div>
+                    <TextInput id="goal_name" type="text" required value={goalName} onChange={(e) => setGoalName(e.target.value)} />
+                </div>
+                <div>
+                    <div className="mb-2 block">
+                        <Label htmlFor="tracking_type" value="Tracking type" />
+                    </div>
+                    <Select id="tracking_type" required value={goalType} onChange={(e) => setGoalType(e.target.value)}>
+                        <option value="simple">Simple count</option>
+                        <option value="minute">Minute count</option>
+                        <option value="step">Step count</option>
+                    </Select>
+                </div>
+                
+                <div>
+                    <div className="mb-2 block">
+                        <Label htmlFor="goal_track" value="Set a goal" />
+                    </div>
+                    <div className="flex flex-row items-center gap-2">
+                        <p>At least</p>
+                        <TextInput id="goal_track" type="text" placeholder="30" required value={goal} onChange={(e) => setGoal(e.target.value)}/>
+                        {goalType === "simple" &&
+                            <p>times per day</p>
+                        }
+                        {goalType === "minute" &&
+                            <p>minutes per day</p>
+                        }
+                        {goalType === "step" &&
+                            <p>steps per day</p>
+                        }
+                    </div>
+                </div>
+
+                <Button>Submit</Button>
+                <Button color="failure" onClick={handleDeletion}><MdDelete className="w-6 h-6" />Delete</Button>
+            </div>
+        </div>
+    )
+}
+
+export default EditGoal
--- src/pages/Goals.jsx
+++ src/pages/Goals.jsx
@@ -6,10 +6,13 @@ import { Badge, Card, Button, Tooltip, Drawer } from 'flowbite-react';
 import { MdOutlineModeEditOutline } from "react-icons/md";
 import { IoIosAdd } from "react-icons/io";
 import { GoGoal } from "react-icons/go";
+import EditGoal from '../components/EditGoal';
 
 function Goals() {
     const goals = useLiveQuery(() => db.goals.toArray());
     const [openDrawer, setOpenDrawer] = useState(false);
+    const [openEditDrawer, setOpenEditDrawer] = useState(false);
+    const [editID, setEditID] = useState(0);
 
     return (
         <div className="container mx-auto max-w-2xl py-8">
@@ -26,7 +29,7 @@ function Goals() {
                                 {item.name}
                             </h5>
                             <Tooltip content="Edit" placement="bottom">
-                                <Button size='xs' outline pill color="light"><MdOutlineModeEditOutline className="h-6 w-6"/></Button>
+                                <Button size='xs' outline pill color="light" onClick={() => (setEditID(item.id), setOpenEditDrawer(true))}><MdOutlineModeEditOutline className="h-6 w-6"/></Button>
                             </Tooltip>
                         </div>
                         <div className="flex">
@@ -49,6 +52,14 @@ function Goals() {
                 <Drawer.Items>
                     <div className="container mx-auto">
                         <AddGoal onSuccess={() => setOpenDrawer(false)}/>
+                    </div>
+                </Drawer.Items>
+            </Drawer>
+            <Drawer open={openEditDrawer} onClose={() => setOpenEditDrawer(false)} position="bottom" className="h-4/6">
+                <Drawer.Header title="Edit goal" titleIcon={MdOutlineModeEditOutline} />
+                <Drawer.Items>
+                    <div className="container mx-auto">
+                        <EditGoal id={editID} onSuccessfulDeletion={() => setOpenEditDrawer(false)}/>
                     </div>
                 </Drawer.Items>
             </Drawer>
