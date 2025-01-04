b8305c609a34d9281478fad5a89b590199f8f48b
Andreas Backström <mozzo242@gmail.com>
2025-01-04 16:31:18 +0100

    Add reflections table, remove moods table

--- src/db.jsx
+++ src/db.jsx
@@ -2,8 +2,8 @@ import Dexie from 'dexie';
 
 export const db = new Dexie('reflection-db');
 db.version(1).stores({
-    moods: '++id, mood, date'
-});
-db.version(1).stores({
     goals: '++id, name, type, goal'
+});
+db.version(2).stores({
+    reflections: '++id, date, metGoals, unMetGoals, mood'
 });
