d1748785b764d62c18d26853c41844236b94dc8f
Andreas Backström <mozzo242@gmail.com>
2025-01-08 16:54:27 +0100

    Add export database functionality

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -2,10 +2,28 @@ import React, { useState } from 'react'
 import { MdOutlineNavigateNext } from "react-icons/md";
 import { Button, Drawer } from 'flowbite-react'
 import ChangeName from '../components/ChangeName';
+import {importDB, exportDB, importInto, peakImportFile} from "dexie-export-import";
+import { db } from '../db';
 
 function Settings() {
     const [openNameDrawer, setOpenNameDrawer] = useState(false);
-        
+    
+    const exportDatabase =  async () => {
+        const blob = await exportDB(db);
+        const href = URL.createObjectURL(blob);
+
+        // create "a" HTML element with href to file & click
+        const link = document.createElement('a');
+        link.href = href;
+        link.setAttribute('download', "db.json"); //or any other extension
+        document.body.appendChild(link);
+        link.click();
+
+        // clean up "a" element & remove ObjectURL
+        document.body.removeChild(link);
+        URL.revokeObjectURL(href);
+    }
+
     return (
         <div className="container mx-auto max-w-2xl">
             <div className="flex flex-col gap-2">
@@ -15,7 +33,7 @@ function Settings() {
                     Change name
                     <MdOutlineNavigateNext className="ml-2 h-5 w-5"/>
                 </Button>
-                <Button outline color="light">
+                <Button outline color="light" onClick={() => exportDatabase()}>
                     Export/import data
                     <MdOutlineNavigateNext className="ml-2 h-5 w-5"/>
                 </Button>
